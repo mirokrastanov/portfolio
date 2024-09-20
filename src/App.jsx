98159a9096f49b697a26dd8888c1c9aa23e0f7a6
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-20 22:44:48 +0300

    Add gradient and projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
+import BackgroundGradient from './components/util/BackgroundGradient';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Technologies from './components/Technologies';
 import Experience from './components/Experience';
+import Projects from './components/Projects';
 
 const App = () => {
   return (
     <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
-      {/* BACKGROUND GRADIENT from: https://bg.ibelick.com/ */}
-      <div className='fixed top-0 -z-10 h-full w-full'>
-        <div className='absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'></div>
-      </div>
+      <BackgroundGradient />
 
-      {/* CONTENT */}
       <div className='container mx-auto px-8 max-md-500:px-4 max-md-300:ml-0'>
         <Navbar />
         <Hero />
         <About />
         <Technologies />
         <Experience />
+        <Projects />
       </div>
     </div>
   )
