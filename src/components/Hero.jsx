136d837bd546fab5bdea99e5bc138714e4c925a2
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-20 18:26:32 +0300

    Update semantic HTML structure and responsiveness.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,24 +5,25 @@ import profilePic from '../assets/profile.png';
 const Hero = () => {
     return (<>
         <div className='border-b border-neutral-900 pb-4 lg:mb-35'>
-            <div className='flex flex-wrap'>
-                <div className='w-full lg:w-1/2'>
+            <article className='flex flex-wrap'>
+                <section className='w-full lg:w-1/2'>
                     <div className='flex flex-col items-center lg:items-start'>
-                        <h1 className='pb-16 text-6xl font-thin lg:mt-16 lg:text-[5rem]'>
+                        <h1 className='pb-16 text-6xl font-thin lg:mt-16 lg:text-[4.9rem]'>
                             Miro Krastanov
                         </h1>
                         <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>
                             Full Stack Developer
                         </span>
-                        <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>
+                        <p className='my-2 max-w-xl py-6 font-light tracking-tight'>{HERO_CONTENT}</p>
                     </div>
-                </div>
-                <div className='w-full lg:w-1/2 lg:p-8'>
+                </section>
+
+                <section className='w-full lg:w-1/2 lg:p-8'>
                     <div className='flex justify-center rounded-2xl p-1'>
                         <img src={profilePic} alt='Miro Krastanov' className='rounded-2xl opacity-85' />
                     </div>
-                </div>
-            </div>
+                </section>
+            </article>
         </div>
 
     </>)
