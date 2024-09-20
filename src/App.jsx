a245be4ed4474fd453765d2bfba048680c50383e
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-20 18:26:55 +0300

    Create about section and incorporate it into the website.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import About from './components/About';
 
 const App = () => {
   return (
-    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 cursor-auto'>
-      <div className='fixed top-0 z-10 h-full w-full'></div>
-      <div className='absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'></div>
+    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
+      {/* BACKGROUND GRADIENT from: https://bg.ibelick.com/ */}
+      <div className='fixed top-0 -z-10 h-full w-full'>
+        <div className='absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'></div>
+      </div>
 
+      {/* CONTENT */}
       <div className='container mx-auto px-8 max-md-300:px-0 max-md-300:ml-0'>
         <Navbar />
         <Hero />
+        <About />
       </div>
     </div>
   )
---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import aboutImg from '../assets/about5.png';
+import { ABOUT_TEXT } from '../constants';
+
+const About = () => {
+    return (<>
+        <div className='border-b border-neutral-900 pb-4'>
+            <h2 className='my-20 text-center text-4xl'>
+                About
+                <span className='text-neutral-500'> Me</span>
+            </h2>
+
+            <article className='flex flex-wrap'>
+                <section className='w-full lg:w-1/2 lg:p-8'>
+                    <div className='flex items-center justify-center'>
+                        <img src={aboutImg} alt='about' className='rounded-2xl' />
+                    </div>
+                </section>
+
+                <section className='w-full lg:w-1/2'>
+                    <div className='flex justify-center lg:justify-start'>
+                        <p className='my-2 max-w-xl py-6 font-light tracking-tight'>{ABOUT_TEXT}</p>
+                    </div>
+                </section>
+            </article>
+        </div>
+    </>)
+}
+
+export default About;
