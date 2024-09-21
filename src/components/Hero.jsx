2aa2b3ace32193ee3c391af5805157cad29866be
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-21 13:02:35 +0300

    Animate Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { HERO_CONTENT } from '../constants';
 import profilePic from '../assets/profile.png';
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+    hidden: { x: -100, opacity: 0 },
+    visible: { x: 0, opacity: 1, transition: { duration: 0.5, delay, }, },
+});
 
 const Hero = () => {
     return (
@@ -8,19 +14,31 @@ const Hero = () => {
             <article className='flex flex-wrap'>
                 <section className='w-full lg:w-1/2'>
                     <div className='flex flex-col items-center lg:items-start'>
-                        <h1 className='pb-16 text-6xl font-thin lg:mt-16 lg:text-[4.9rem]'>
+                        <motion.h1
+                            className='pb-16 text-6xl font-thin lg:mt-16 lg:text-[4.9rem]'
+                            variants={container(0)} initial={`hidden`} animate={`visible`}>
                             Miro Krastanov
-                        </h1>
-                        <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>
+                        </motion.h1>
+                        <motion.span
+                            className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'
+                            variants={container(0.5)} initial={`hidden`} animate={`visible`}>
                             Full Stack Developer
-                        </span>
-                        <p className='my-2 max-w-xl py-6 font-light tracking-tight'>{HERO_CONTENT}</p>
+                        </motion.span>
+                        <motion.p
+                            className='my-2 max-w-xl py-6 font-light tracking-tight'
+                            variants={container(1)} initial={`hidden`} animate={`visible`}>
+                            {HERO_CONTENT}
+                        </motion.p>
                     </div>
                 </section>
 
                 <section className='w-full lg:w-1/2 lg:p-8'>
                     <div className='flex justify-center rounded-2xl p-1'>
-                        <img src={profilePic} alt='Miro Krastanov' className='rounded-2xl opacity-85' />
+                        <motion.img
+                            src={profilePic} alt='Miro Krastanov' className='rounded-2xl opacity-85'
+                            initial={{ x: 100, opacity: 0 }} animate={{ x: 0, opacity: 1 }}
+                            transition={{ duration: 1, delay: 1.2 }}
+                        />
                     </div>
                 </section>
             </article>
