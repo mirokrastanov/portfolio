81c1b801835a69c31531dc0eb1b66532246991f7
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-21 15:14:28 +0300

    Animate Experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,17 +9,23 @@ const Experience = () => {
             <motion.h1
                 className='my-20 text-center text-4xl'
                 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }}
-                transition={{ duration: 0.5 }}>
+                transition={{ duration: 1 }}>
                 Experience
             </motion.h1>
             <div>
                 {EXPERIENCES.map((xp, i) => (
                     <article key={i} className='mb-8 flex flex-wrap lg:justify-center' >
-                        <section className='w-full lg:w-1/4'>
+                        <motion.section
+                            className='w-full lg:w-1/4'
+                            whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }}
+                            transition={{ duration: 1 }}>
                             <p className='mb-2 text-sm text-neutral-400'>{xp.year}</p>
-                        </section>
+                        </motion.section>
 
-                        <section className='w-full max-w-xl lg:w-3/4'>
+                        <motion.section
+                            className='w-full max-w-xl lg:w-3/4'
+                            whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }}
+                            transition={{ duration: 1 }}>
                             <h6 className='mb-2 font-semibold'>
                                 {xp.role} -
                                 <span className='text-sm text-purple-100'> {xp.company}</span>
@@ -27,12 +33,14 @@ const Experience = () => {
                             <p className='mb-4 text-neutral-400'>{xp.description}</p>
                             <div className='flex flex-wrap gap-2'>
                                 {xp.technologies.map((tech, i) => (
-                                    <span key={i} className='rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-500 border border-neutral-800'>
+                                    <motion.span key={i} className='rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-500 border border-neutral-800'
+                                        whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 200 }}
+                                        transition={{ duration: 1, delay: Number(`0.${5 + i}`) }}>
                                         {tech}
-                                    </span>
+                                    </motion.span>
                                 ))}
                             </div>
-                        </section>
+                        </motion.section>
                     </article>
                 ))}
             </div>
