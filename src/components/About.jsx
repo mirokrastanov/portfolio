15c7e97dfe8a855a0071d1efee962804b9b8af9d
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-21 13:07:56 +0300

    Animate About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import aboutImg from '../assets/about5.png';
 import { ABOUT_TEXT } from '../constants';
+import { motion } from "framer-motion";
 
 const About = () => {
     return (
@@ -11,17 +12,22 @@ const About = () => {
             </h2>
 
             <article className='flex flex-wrap'>
-                <section className='w-full lg:w-1/2 lg:p-8'>
+                <motion.section
+                    className='w-full lg:w-1/2 lg:p-8'
+                    whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 0.5 }} >
                     <div className='flex items-center justify-center'>
                         <img src={aboutImg} alt='about' className='rounded-2xl' />
                     </div>
-                </section>
+                </motion.section>
 
-                <section className='w-full lg:w-1/2'>
+                <motion.section className='w-full lg:w-1/2'
+                    whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }}
+                    transition={{ duration: 0.5 }} >
                     <div className='flex justify-center lg:justify-start'>
                         <p className='my-2 max-w-xl py-6 font-light tracking-tight'>{ABOUT_TEXT}</p>
                     </div>
-                </section>
+                </motion.section>
             </article>
         </div>
     )
