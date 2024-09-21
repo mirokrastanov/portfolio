990baf8452bb4318e94140317b48aa904e03a03b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-21 15:22:24 +0300

    Animate Projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,30 +1,45 @@
 import React from 'react';
 import { PROJECTS } from '../constants';
+import { motion } from "framer-motion";
+
 
 const Projects = () => {
     return (
         <div className='border-b border-neutral-900 pb-4'>
-            <h2 className='my-20 text-center text-4xl'>Projects</h2>
+            <motion.h2
+                className='my-20 text-center text-4xl'
+                whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 1 }}>
+                Projects
+            </motion.h2>
 
-            <div className=''>
+            <div>
                 {PROJECTS.map((p, i) => (
-                    <div key={i} className='mb-8 flex flex-wrap lg:justify-center'>
-                        <div className='w-full lg:w-1/4'>
+                    <article key={i} className='mb-8 flex flex-wrap lg:justify-center'>
+                        <motion.section
+                            className='w-full lg:w-1/4'
+                            whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }}
+                            transition={{ duration: 1 }}>
                             <img src={p.image} alt={p.title} width={150} height={150} className='mb-6 rounded-xl' />
-                        </div>
-                        <div className='w-full max-w-xl lg:w-3/4'>
+                        </motion.section>
+                        <motion.section
+                            className='w-full max-w-xl lg:w-3/4'
+                            whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }}
+                            transition={{ duration: 1 }}>
                             <h6 className='mb-2 font-semibold'>{p.title}</h6>
                             <h6 className='mb-4 text-neutral-400'>{p.description}</h6>
 
                             <div className='flex flex-wrap gap-2'>
                                 {p.technologies.map((tech, i) => (
-                                    <span key={i} className='rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-500 border border-neutral-800'>
+                                    <motion.span key={i} className='rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-500 border border-neutral-800'
+                                        whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 200 }}
+                                        transition={{ duration: 1, delay: Number(`0.${5 + i}`) }}>
                                         {tech}
-                                    </span>
+                                    </motion.span>
                                 ))}
                             </div>
-                        </div>
-                    </div>
+                        </motion.section>
+                    </article>
                 ))}
             </div>
         </div>
