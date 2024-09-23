e85a551f503e1cacf423cb49f448db962dcf7063
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-23 14:54:05 +0300

    Animate the contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,30 +20,45 @@ const Contact = () => {
                     className='flex flex-col max-w-[600px] w-full'
                 >
                     <div className=''>
-                        <p className='text-3xl font-bold inline border-b-4 border-pink-600 text-neutral-300'>
+                        <motion.p
+                            className='text-3xl font-bold inline-block border-b-4 border-pink-600 text-neutral-300'
+                            whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }}
+                            transition={{ duration: 1 }}>
                             Contact
-                        </p>
-                        <p className='text-neutral-300 py-4'>
+                        </motion.p>
+                        <motion.p
+                            className='text-neutral-300 py-4'
+                            whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }}
+                            transition={{ duration: 1, delay: 0.2 }}>
                             Submit the form below or shoot me an email -
                             <span className='ml-1 border-b-2 border-pink-600'>
-                                <Link to={'mailto:m.krastanov@outlook.com'}>m.krastanov@outlook.com</Link>
+                                <Link to={`mailto:${CONTACT.email}`}>{CONTACT.email}</Link>
                             </span>
-                        </p>
-                        <input
+                        </motion.p>
+                        <motion.input
                             type='text' placeholder='Name' name='name' id='form-name' autoComplete='off'
                             className='p-2 bg-slate-200 w-full rounded text-black outline-none active:outline-none'
+                            whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }}
+                            transition={{ duration: 1, delay: 0.4 }}
                         />
-                        <input
+                        <motion.input
                             type='email' placeholder='Email' name='email' id='form-email' autoComplete='off'
                             className='my-4 p-2 bg-slate-200 w-full rounded text-black outline-none active:outline-none'
+                            whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }}
+                            transition={{ duration: 1, delay: 0.6 }}
                         />
-                        <textarea
+                        <motion.textarea
                             name='message' placeholder='Message' id='form-msg' rows='10'
                             className='w-full p-2 bg-slate-200 rounded text-black outline-none active:outline-none'
-                        ></textarea>
-                        <button type='submit' className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 mt-8 mx-auto flex items-center rounded'>
+                            whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 80 }}
+                            transition={{ duration: 1, delay: 0.6 }}
+                        ></motion.textarea>
+                        <motion.button type='submit'
+                            className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 mt-8 mx-auto flex items-center rounded'
+                            whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }}
+                            transition={{ duration: 0.5, delay: 1 }}>
                             Let's Collaborate
-                        </button>
+                        </motion.button>
                     </div>
                 </form>
             </article>
