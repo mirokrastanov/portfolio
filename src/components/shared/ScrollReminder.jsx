2a2bd96f099996fd91a21c0a7f00be9a5db0ecd3
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-23 14:26:48 +0300

    Create scroll reminder icon with bouncing animation to indicate scrolling is availabe at the top.

---
+++ src/components/shared/ScrollReminder.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { CgScrollV, CgMouse } from "react-icons/cg";
+
+
+const ScrollReminder = () => {
+    const [hasScrolled, setHasScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >= 10) setHasScrolled(true);
+            else setHasScrolled(false);
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+
+    return (
+        <div className='absolute bg-transparent right-0 top-1/2 -translate-y-1/2'>
+            {!hasScrolled && <CgScrollV className='text-white top-0 right-0 text-4xl animate-bounce' />}
+        </div>
+    )
+}
+
+export default ScrollReminder;
