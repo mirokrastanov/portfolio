8071776f49ca0178a49298f61d0a992ac5ca3ecc
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-23 22:58:45 +0300

    Footer now fully functional.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import logo from '../assets/logo.png';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import { TbMailFilled } from "react-icons/tb";
-import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaFileDownload } from "react-icons/fa";
 import NavIconWrapper from './shared/NavIconWrapper';
-
 
 const Footer = () => {
     // Responsive styles - popped out for HTML clarity
@@ -20,6 +19,20 @@ const Footer = () => {
         i: 'max-md-500:mr-0'
     };
 
+    const URLs = {
+        LinkedIn: 'https://www.linkedin.com/in/mkrastanov/',
+        GitHub: 'https://github.com/mirokrastanov',
+        Email: 'mailto:m.krastanov@outlook.com',
+        Resume: 'https://drive.google.com/uc?export=download&id=1ZLKdD4Dm_8UsiFFb3ubMgr_3GGTMCTGh',
+    };
+    // TODO: Add those to constants
+
+    const handleNavBtnCLick = (e, tooltip) => {
+        e.preventDefault();
+        const URL = URLs[tooltip];
+        window.open(URL, '_blank', 'noopener,noreferrer')
+    };
+
     return (
         <footer className='relative flex items-center justify-between py-4'>
             <div className='flex flex-shrink-0 items-center'>
@@ -27,10 +40,14 @@ const Footer = () => {
             </div>
 
             <div className={`m-4 flex items-center justify-center gap-4 max-md-300:gap-x-3 max-md-250:gap-x-4 text-2xl ${sub250.i} ${sub300.i} ${sub500.i}`}>
-                <NavIconWrapper Icon={FaLinkedin} tooltip='LinkedIn' custom='' footer={true} />
-                <NavIconWrapper Icon={FaGithub} tooltip='GitHub' custom='' footer={true} />
-                <NavIconWrapper Icon={TbMailFilled} tooltip='Email' custom='' footer={true} incSize={true} />
-                <NavIconWrapper Icon={BsFillPersonLinesFill} tooltip='Resume' custom='' footer={true} incSize={true} />
+                <NavIconWrapper Icon={FaLinkedin} tooltip='LinkedIn' footer={true}
+                    custom='' handler={(e) => handleNavBtnCLick(e, 'LinkedIn')} />
+                <NavIconWrapper Icon={FaGithub} tooltip='GitHub' footer={true}
+                    custom='' handler={(e) => handleNavBtnCLick(e, 'GitHub')} />
+                <NavIconWrapper Icon={TbMailFilled} tooltip='Email' custom='' footer={true}
+                    incSize={true} handler={(e) => handleNavBtnCLick(e, 'Email')} />
+                <NavIconWrapper Icon={FaFileDownload} tooltip='Resume' custom='' footer={true}
+                    handler={(e) => handleNavBtnCLick(e, 'Resume')} />
             </div>
         </footer>
     )
